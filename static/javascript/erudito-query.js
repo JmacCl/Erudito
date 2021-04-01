deea5fab0bd426680c0a4249397226724068203f
James McClure <2385464M@student.gla.ac.uk>
2021-04-01 10:38:44 +0100

    Fixed issue where text changes colour

--- static/javascript/erudito-query.js
+++ static/javascript/erudito-query.js
@@ -6,10 +6,10 @@ $(document).ready(function () {
     });
 });
 
-$('p').hover(
-    function() {
-        $(this).css('color', 'red');
-    },
-    function() {
-        $(this).css('color', 'white');
-    });
+// $('p').hover(
+//     function() {
+//         $(this).css('color', 'red');
+//     },
+//     function() {
+//         $(this).css('color', 'white');
+//     });
